81b40267d4d9413e963dc999953e12a24797ab90
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2015-11-30 23:26:34 +0300

    lesson 15: done

--- app/__tests__/index-test.js
+++ app/__tests__/index-test.js
@@ -1,52 +1,9 @@
 jest.dontMock('../src');
+jest.autoMockOff();
 const deepFreeze = require('deep-freeze');
-const todos = require('../src/index.js');
+const store = require('../src/index.js');
 describe('counter tests', () => {
-
-    it('test freezed reducer', () => {
-        const stateBefore = [];
-        const action = {
-            type: 'ADD_TODO',
-            id: 0,
-            text: 'learn redux'
-        };
-        const stateAfter = [
-            {
-                id: 0,
-                text: 'learn redux',
-                completed: false
-            }
-        ];
-        deepFreeze(stateBefore);
-        deepFreeze(action);
-        expect(todos(stateBefore, action)).toEqual(stateAfter);
-    });
-    it('test toggle todo reducer', () => {
-        const stateBefore = [{
-            id: 0,
-            text: 'learn redux',
-            completed: false
-        }, {
-            id: 1,
-            text: 'go shopping',
-            completed: false
-        }];
-        const action = {
-            type: 'TOGGLE_TODO',
-            id: 1
-        };
-        const stateAfter = [{
-            id: 0,
-            text: 'learn redux',
-            completed: false
-        }, {
-            id: 1,
-            text: 'go shopping',
-            completed: true
-        }];
-        deepFreeze(stateBefore);
-        deepFreeze(action);
-
-        expect(todos(stateBefore, action)).toEqual(stateAfter);
-    });
+    it('should work well in case of combine reducers', () => {
+        console.log(store.getState());
+    })
 });
--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,12 @@
-const todo = (state,action ) => {
+import {createStore, combineReducers} from 'redux';
+const todo = (state, action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                };
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
         case 'TOGGLE_TODO':
             if (state.id !== action.id) {
                 return state;
@@ -19,20 +20,30 @@ const todo = (state,action ) => {
     }
 };
 
-const todos = (state = [], action) => {
+const todos = (state = [], action = undefined) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-               todo(undefined, action)
+                todo(undefined, action)
             ];
         case 'TOGGLE_TODO':
         {
-            return state.map(t => todo(t,action));
+            return state.map(t => todo(t, action));
         }
         default :
             return state;
     }
 };
 
-module.exports = todos;
+const visibilityFilter = (state = 'SHOW_ALL', action = undefined) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+const todoApp = combineReducers({todos: todos, visibilityFilter: visibilityFilter});
+const store = createStore(todoApp);
+module.exports = store;
