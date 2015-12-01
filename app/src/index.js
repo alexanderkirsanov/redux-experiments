f81c69ef101dd30cbe80f94ce2ecd9f6d8e0bd0b
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2015-12-02 00:35:56 +0300

    lesson 20: create separate component

--- app/src/index.js
+++ app/src/index.js
@@ -56,7 +56,7 @@ const FilterLink = ({
     currentFilter,
     children
     }) => {
-    if (filter === currentFilter){
+    if (filter === currentFilter) {
         return <span>{children}</span>;
     }
     return (
@@ -83,6 +83,30 @@ const getVisibleTodos = (todos, filter) => {
             return todos.filter(t=>!t.completed);
     }
 }
+const Todo = ({
+    onClick,
+    completed,
+    text
+    }) => (
+    <li onClick={onClick}
+        style={{textDecoration: completed ? 'line-through': 'none'}}>
+        {text}
+    </li>
+);
+const TodoList = ({
+    todos,
+    onTodoClick
+    }) => (
+    <ul>
+        {todos.map(todo =>
+                <Todo
+                    key={todo.id}
+                    {...todo}
+                      onClick={() => {onTodoClick(todo.id)}}
+                    />
+        )}
+    </ul>
+);
 class TodoApp extends Component {
     render() {
         const {todos, visibilityFilter} = this.props;
@@ -102,20 +126,9 @@ class TodoApp extends Component {
                 }}>
                     Add Todo
                 </button>
-                <ul>{visibleTodos.map(todo =>
-                        <li key={todo.id}
-                            onClick={() => {
-                                store.dispatch({
-                                    type: 'TOGGLE_TODO',
-                                    id: todo.id
-                                });
-                            }}
-                            style={{textDecoration:
-                                todo.completed ? 'line-through': 'none'}}>
-                            {todo.text}
-                        </li>
-                )}
-                </ul>
+                <TodoList todos={visibleTodos}
+                          onTodoClick={id => store.dispatch({type:'TOGGLE_TODO', id})}
+                    />
                 <p>
                     Show:
                     {' '} <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}> All </FilterLink>
